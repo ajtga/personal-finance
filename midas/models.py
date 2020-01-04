f60b68a112c9412ab445f3ea7b8fd1f413fa5329
Augusto José <ajtgalencar@gmail.com>
2020-01-04 17:32:17 -0300

    Add dependent models

--- midas/models.py
+++ midas/models.py
@@ -42,3 +42,59 @@ class Currencies(models.Model):
     iso_code = models.CharField(max_length=3, verbose_name='ISO 4217 code')
     symbol = models.CharField(max_length=255, verbose_name='currency symbol')
     name = models.CharField(max_length=255, verbose_name='currency name')
+
+
+class Accounts(models.Model):
+    account_id = models.AutoField(primary_key=True)
+    holder_id = models.ForeignKey(AccountHolders, on_delete=models.CASCADE)
+    current_balance = models.FloatField(verbose_name='current balance')
+    last_balance_update = models.DateTimeField(verbose_name='last balance update')
+    account_name = models.CharField(max_length=255, verbose_name='account name')
+
+
+class Affiliations(models.Model):
+    affiliation_id = models.AutoField(primary_key=True)
+    person = models.ForeignKey(People, on_delete=models.CASCADE)
+    group = models.ForeignKey(Groups, on_delete=models.CASCADE)
+
+
+class MonthlyBalances(models.Model):
+    balance_id = models.AutoField(primary_key=True)
+    account_id = models.ForeignKey(Accounts, on_delete=models.CASCADE)
+    balance = models.FloatField(verbose_name='balance')
+    month = models.DateField(verbose_name='month')
+
+
+class Subcategories(models.Model):
+    subcategory_id = models.AutoField(primary_key=True)
+    category_id = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, verbose_name='subcategory name')
+
+
+class Budget(models.Model):
+    limit_id = models.AutoField(primary_key=True)
+    subcategory_id = models.ForeignKey(Subcategories, on_delete=models.CASCADE)
+    month_limit = models.FloatField(default=0.0, verbose_name='month limit')
+
+
+class Movements(models.Model):
+    movement_id = models.AutoField(primary_key=True)
+    type = models.ForeignKey(MovementType, on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(Subcategories, on_delete=models.CASCADE)
+    currency = models.ForeignKey(Currencies, on_delete=models.CASCADE)
+    movement_method = models.ForeignKey(MovementMethods, on_delete=models.CASCADE)
+    account = models.ForeignKey(Accounts, on_delete=models.CASCADE)
+    value = models.FloatField(verbose_name='movement value')
+
+
+class Annotations(models.Model):
+    annotation_id = models.AutoField(primary_key=True)
+    movement = models.ForeignKey(Movements, on_delete=models.CASCADE)
+    annotation_text = models.CharField(max_length=255, verbose_name='annotation text')
+    annotation_datetime = models.DateTimeField(verbose_name='annotation date and time')
+
+
+class MovementBy(models.Model):
+    bridge_id = models.AutoField(primary_key=True)
+    movement = models.ForeignKey(Movements, on_delete=models.CASCADE)
+    affiliation = models.ForeignKey(Affiliations, on_delete=models.CASCADE)
