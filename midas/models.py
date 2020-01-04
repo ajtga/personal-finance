2a3aded8b9f46b9fdbb15532813255fa00f21bfb
Augusto José <ajtgalencar@gmail.com>
2020-01-04 17:40:38 -0300

    Revert "Sort models"
    This reverts commit e51a8cf781378cf3ddf408acbd2a4ab3a6261a0a.

--- midas/models.py
+++ midas/models.py
@@ -1,63 +1,6 @@
 from django.db import models
 
 
-class Accounts(models.Model):
-    account_id = models.AutoField(primary_key=True)
-    holder_id = models.ForeignKey(AccountHolders, on_delete=models.CASCADE)
-    current_balance = models.FloatField(verbose_name='current balance')
-    last_balance_update = models.DateTimeField(verbose_name='last balance update')
-    account_name = models.CharField(max_length=255, verbose_name='account name')
-
-
-class AccountHolders(models.Model):
-    holder_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255, verbose_name='account holder name')
-    code = models.CharField(max_length=3, verbose_name='account holder code')
-
-
-class Affiliations(models.Model):
-    affiliation_id = models.AutoField(primary_key=True)
-    person = models.ForeignKey(People, on_delete=models.CASCADE)
-    group = models.ForeignKey(Groups, on_delete=models.CASCADE)
-
-
-class Annotations(models.Model):
-    annotation_id = models.AutoField(primary_key=True)
-    movement = models.ForeignKey(Movements, on_delete=models.CASCADE)
-    annotation_text = models.CharField(max_length=255, verbose_name='annotation text')
-    annotation_datetime = models.DateTimeField(verbose_name='annotation date and time')
-
-
-class Budget(models.Model):
-    limit_id = models.AutoField(primary_key=True)
-    subcategory_id = models.ForeignKey(Subcategories, on_delete=models.CASCADE)
-    month_limit = models.FloatField(default=0.0, verbose_name='month limit')
-
-
-class Categories(models.Model):
-    category_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255, verbose_name='category name')
-
-
-class Currencies(models.Model):
-    currency_id = models.AutoField(primary_key=True)
-    iso_code = models.CharField(max_length=3, verbose_name='ISO 4217 code')
-    symbol = models.CharField(max_length=255, verbose_name='currency symbol')
-    name = models.CharField(max_length=255, verbose_name='currency name')
-
-
-class Groups(models.Model):
-    group_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255, verbose_name='group name')
-
-
-class MonthlyBalances(models.Model):
-    balance_id = models.AutoField(primary_key=True)
-    account_id = models.ForeignKey(Accounts, on_delete=models.CASCADE)
-    balance = models.FloatField(verbose_name='balance')
-    month = models.DateField(verbose_name='month')
-
-
 class MovementMethods(models.Model):
     name = models.CharField(primary_key=True, max_length=255)
 
@@ -72,10 +15,66 @@ class MovementType(models.Model):
     sign = models.IntegerField(default=1, choices=SIGN_OPTIONS)
 
 
-class MovementBy(models.Model):
-    bridge_id = models.AutoField(primary_key=True)
-    movement = models.ForeignKey(Movements, on_delete=models.CASCADE)
-    affiliation = models.ForeignKey(Affiliations, on_delete=models.CASCADE)
+class Categories(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, verbose_name='category name')
+
+
+class People(models.Model):
+    person_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=255, verbose_name='first name')
+    last_name = models.CharField(max_length=255, verbose_name='last name')
+
+
+class Groups(models.Model):
+    group_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, verbose_name='group name')
+
+
+class AccountHolders(models.Model):
+    holder_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, verbose_name='account holder name')
+    code = models.CharField(max_length=3, verbose_name='account holder code')
+
+
+class Currencies(models.Model):
+    currency_id = models.AutoField(primary_key=True)
+    iso_code = models.CharField(max_length=3, verbose_name='ISO 4217 code')
+    symbol = models.CharField(max_length=255, verbose_name='currency symbol')
+    name = models.CharField(max_length=255, verbose_name='currency name')
+
+
+class Accounts(models.Model):
+    account_id = models.AutoField(primary_key=True)
+    holder_id = models.ForeignKey(AccountHolders, on_delete=models.CASCADE)
+    current_balance = models.FloatField(verbose_name='current balance')
+    last_balance_update = models.DateTimeField(verbose_name='last balance update')
+    account_name = models.CharField(max_length=255, verbose_name='account name')
+
+
+class Affiliations(models.Model):
+    affiliation_id = models.AutoField(primary_key=True)
+    person = models.ForeignKey(People, on_delete=models.CASCADE)
+    group = models.ForeignKey(Groups, on_delete=models.CASCADE)
+
+
+class MonthlyBalances(models.Model):
+    balance_id = models.AutoField(primary_key=True)
+    account_id = models.ForeignKey(Accounts, on_delete=models.CASCADE)
+    balance = models.FloatField(verbose_name='balance')
+    month = models.DateField(verbose_name='month')
+
+
+class Subcategories(models.Model):
+    subcategory_id = models.AutoField(primary_key=True)
+    category_id = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, verbose_name='subcategory name')
+
+
+class Budget(models.Model):
+    limit_id = models.AutoField(primary_key=True)
+    subcategory_id = models.ForeignKey(Subcategories, on_delete=models.CASCADE)
+    month_limit = models.FloatField(default=0.0, verbose_name='month limit')
 
 
 class Movements(models.Model):
@@ -88,13 +87,14 @@ class Movements(models.Model):
     value = models.FloatField(verbose_name='movement value')
 
 
-class People(models.Model):
-    person_id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=255, verbose_name='first name')
-    last_name = models.CharField(max_length=255, verbose_name='last name')
+class Annotations(models.Model):
+    annotation_id = models.AutoField(primary_key=True)
+    movement = models.ForeignKey(Movements, on_delete=models.CASCADE)
+    annotation_text = models.CharField(max_length=255, verbose_name='annotation text')
+    annotation_datetime = models.DateTimeField(verbose_name='annotation date and time')
 
 
-class Subcategories(models.Model):
-    subcategory_id = models.AutoField(primary_key=True)
-    category_id = models.ForeignKey(Categories, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255, verbose_name='subcategory name')
+class MovementBy(models.Model):
+    bridge_id = models.AutoField(primary_key=True)
+    movement = models.ForeignKey(Movements, on_delete=models.CASCADE)
+    affiliation = models.ForeignKey(Affiliations, on_delete=models.CASCADE)
